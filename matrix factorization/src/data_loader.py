e9afc6c0efad3c32f0627cec6e9d7e4f14c374ba
Zhanna Orlova <orlova.zhan@gmail.com>
2024-05-19 17:35:46 -0600

    data loader final changes

--- matrix factorization/src/data_loader.py
+++ matrix factorization/src/data_loader.py
@@ -8,21 +8,33 @@ from datetime import date
 
 class Dataset:
     def __init__(self, API_command):
+        # Download the dataset
         os.system(API_command)
-        self.name = API_command.split('/')
+        
+        # Extract the zip filename from the command
+        self.name = API_command.split('-d ')[1].split(' ')[0]
+        self.zip_filename = self.name.split('/')[-1] + '.zip'
+        
         self.Open()
-        os.remove(self.name[1]+'.zip')
+        os.remove(self.zip_filename)
     
     def Open(self):
         self.dataset = []
-        with zipfile.ZipFile(self.name[1]+'.zip', 'r') as zip:
-            filenames = zip.namelist()
-            zip.extractall()
-        for i in filenames:
-            self.dataset.append(pd.read_csv(i))
+        
+        with zipfile.ZipFile(self.zip_filename, 'r') as zip_ref:
+            filenames = zip_ref.namelist()
+            zip_ref.extractall()
+        
+        for file in filenames:
+            if file.endswith('rating.csv'):
+                self.dataset.append(pd.read_csv(file, nrows=10000))
+            else:
+                self.dataset.append(pd.read_csv(file))
         self.filenames = filenames
 
-    def filter_data(self, df):
-        df_subset = df.sample(frac=0.1, random_state=1)
-        return df_subset
-
+# Code to run only if this script is executed directly
+if __name__ == "__main__":
+    data = Dataset('kaggle datasets download -d grouplens/movielens-20m-dataset')
+    
+    for df in data.dataset:
+        print(df.head())  # Printing only the first few rows for verification
